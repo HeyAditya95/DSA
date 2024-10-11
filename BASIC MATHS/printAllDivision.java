08ac63080257b6db7979d33662473e3ab7cae2fc
Aditya jha <haaditya95b@gmail.com>
2024-10-11 23:27:40 +0530

    here are two methods for printing all divisors of a particular method

--- BASIC MATHS/printAllDivision.java
+++ BASIC MATHS/printAllDivision.java
@@ -1,14 +1,27 @@
 import java.util.*;
 public class printAllDivision {
     public static void main(String[] args) {
+        //this have time complexity of O(N)
         Scanner sc = new Scanner(System.in);
         System.out.println("enter the number : ");
         int N = sc.nextInt();
-       for(int i=1;i<=N;i++){
-        if(N % i == 0){
+    //    for(int i=1;i<=N;i++){
+    //     if(N % i == 0){
+    //         System.out.println(i);
+    //     }
+    //    }
+
+    //with another method
+    for(int i=1;i<=Math.sqrt(36);i++)
+    {
+        if(N%i==0){
             System.out.println(i);
+            if(N/i != i)
+        {
+            System.out.println(N/i);
         }
-       }
+        } 
+    }
 
     }
 }
